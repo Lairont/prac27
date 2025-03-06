b2c2005d15043914c12b55a49bd2ff5069fbd131
Никита <153456515+Lairont@users.noreply.github.com>
2025-03-07 01:27:53 +0200

    Add files via upload

---
+++ ConsoleApp3/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public double X 
+        {
+            get => x; 
+            set => x = value; 
+        }
+        public double Y 
+        { 
+            get => y; 
+            set => y = value; 
+        }
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public  double Length()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+        public  override string ToString()
+        {
+            return $"x={X}\ty={Y}";
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point>points = new List<Point>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"x {i + 1} точки: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"y {i + 1} точки: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                points.Add(new Point(x, y));
+                Console.WriteLine();
+            }
+            foreach (var p in points)
+            {
+
+                Console.WriteLine($"{p.ToString()}");
+                p.Length();
+            }
+            Console.ReadLine();
+        }
+    }
+}
