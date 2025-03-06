973417a6251866fc45f379da1728c74b758ed396
Никита <153456515+Lairont@users.noreply.github.com>
2025-03-07 01:36:32 +0200

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            List<Car>cars = new List<Car>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Введите марку авто {i + 1}: ");
+                string make = Console.ReadLine();
+                Console.Write($"Введите модель авто {i + 1}: ");
+                string model = Console.ReadLine();
+                Console.Write($"Введите год выпуска авто {i + 1}: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                cars.Add(make, model, year);
+                Console.WriteLine();
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.ToString()}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
